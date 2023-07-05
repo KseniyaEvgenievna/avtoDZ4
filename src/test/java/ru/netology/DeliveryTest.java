e096da010f26bf508fb0b3b508d0a0143af680aa
KseniyaEvgenievna <xeniya.11ks@gmail.com>
2023-07-05 23:17:59 +0300

    Gitignore added

---
+++ src/test/java/ru/netology/DeliveryTest.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DeliveryTest {
+
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void successfullyСompletedTests() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Пенза");
+        String currentDate = generateDate(4);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(currentDate);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("05.07.2023");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + currentDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
+
+    }
+
+
+}
